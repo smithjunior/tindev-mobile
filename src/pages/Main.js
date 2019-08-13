8167e01ce0e97947a143c73ad5269ef20b78d301
Smith Júnior <smith.junior@icloud.com>
2019-08-12 23:04:39 -0300

    UPDATE: Update main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -71,14 +71,16 @@ export default function Main ({ navigation }) {
           ))
         )}
       </View>
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleDislike}>
-          <Image source={dislike} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleLike}>
-          <Image source={like} />
-        </TouchableOpacity>
-      </View>
+      { devs.length > 0 && (
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleDislike}>
+            <Image source={dislike} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleLike}>
+            <Image source={like} />
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   )
 }
